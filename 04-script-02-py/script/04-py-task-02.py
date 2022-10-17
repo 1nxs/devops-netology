8a85979c2d4819dc5cbb8e74efce4a7db1661858
Pavel Yakushin <yakushin.pavel+netology@gmail.com>
2022-10-17 22:26:26 +0300

    minor fixes

--- 04-script-02-py/script/04-py-task-02.py
+++ 04-script-02-py/script/04-py-task-02.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python3
-
 import os
 
-bash_command = ["cd C:\\GIT\\devops-netology", "git status"]
+target="C:\\GIT\\devops-netology"
+print(f'Target path: {target}')
+bash_command = ["cd "+target, "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
 #is_change = False
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
-        prepare_result = result.replace('\tmodified:   ', '')
-        print(prepare_result)
+        prepare_result = result.replace('\tmodified:   ', '').replace('#','')
+        print(os.path.join(target,prepare_result))
         #break
+    elif result.find('new file') != -1:
+        prepare_result = result.replace('\tnew file:   ', '').replace('#','')
+        print(os.path.join(target, prepare_result))
