239277b4606db2142e73f643b7d56f52797736ec
Pavel Yakushin <yakushin.pavel+netology@gmail.com>
2022-10-18 15:35:26 +0300

    update script

--- 04-script-02-py/script/04-py-task-04.py
+++ 04-script-02-py/script/04-py-task-04.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-import time
 from datetime import datetime
 import socket
+import time
 
 service_host = {
     'drive.google.com': '0',
     'mail.google.com': '0',
     'google.com': '0'
 }
-
 for host in service_host:
     initial_ip = socket.gethostbyname(host)
     service_host[host] = initial_ip
@@ -18,6 +17,8 @@ while True:
         old_ip = service_host[host]
         new_ip = socket.gethostbyname(host)
         if new_ip != old_ip:
-            service_addr[host] = new_ip
-    print(host + " - " + service_host[host])
+            service_host[host] = new_ip
+
+
+        print(host + " - " + service_host[host])
     time.sleep(10)
