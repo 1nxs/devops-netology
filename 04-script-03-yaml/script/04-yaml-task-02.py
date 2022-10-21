5079685d6899ede247c4c37441a998e8bb86e5bd
Pavel Yakushin <yakushin.pavel+netology@gmail.com>
2022-10-21 15:02:23 +0300

    update script

--- 04-script-03-yaml/script/04-yaml-task-02.py
+++ 04-script-03-yaml/script/04-yaml-task-02.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 #
-from datetime import datetime
+import datetime
 import socket
 import time
+import json
+import yaml
 
 # Нули
 service_host = {
@@ -10,15 +12,32 @@ service_host = {
     'mail.google.com': '0',
     'google.com': '0'
 }
+
+# Функция заполнения словаря Актуальными IP адресами
+def fill_tlist(x):
+    for node in x:
+        ipaddres = socket.gethostbyname(node)
+        x[node] = ipaddres
+    return x
+
 # Получаем текущие значения
 for host in service_host:
     initial_ip = socket.gethostbyname(host)
     service_host[host] = initial_ip
 
+# запись в формате json \ yaml
+def wr_json_yaml(y):
+    with open('service_host.json', 'w') as jtmp:
+        jtmp.write(str(json.dumps(y)))
+    with open('service_hosts.yaml', 'w') as ytmp:
+        ytmp.write(yaml.dump(y))
+    return
+
 while True:
-    # Пишем время старта цикла
-    dt = datetime.now()
-    print("'-..-''-..-'  ", dt)
+    # Определяем время для каждого цикла
+    dtn = datetime.datetime.now()
+    d = dtn.strftime('%H:%M:%S')
+    print("-")
     # Перебираем хосты из словаря
     for host in service_host:
         old_ip = service_host[host]
@@ -26,7 +45,9 @@ while True:
         # Проверяем значение, если не совпадает то записываем в словарь и ругаемся ошибкой
         if new_ip != old_ip:
             service_host[host] = new_ip
-            print("[ERROR] "+host+" IP changed: old IP "+old_ip+", new IP "+new_ip)
-            print("[ERROR] Shit happens at >>>>>>", dt)
-        print(host + " - " + service_host[host])
+#            print(d+" [ERROR] "+host+" IP changed: old IP "+old_ip+", new IP "+new_ip)
+            print(d+ " - " +host+ " : " +new_ip+ " <<< [IP changed] - old IP >>> "+old_ip+"")
+            #print("[ERROR] Shit happens at >>>>>>", d)
+        print(d+ " - "+host+ " : " +service_host[host])
+        wr_json_yaml(service_host)
     time.sleep(10)
