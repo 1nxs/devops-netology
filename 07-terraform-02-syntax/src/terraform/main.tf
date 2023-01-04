015eed3b7bd27825e7c042b6b309c161a7ffae8b
inxs <yakushin.pavel+netology@gmail.com>
2023-01-04 21:52:50 +0300

    update homework

--- 07-terraform-02-syntax/src/terraform/main.tf
+++ 07-terraform-02-syntax/src/terraform/main.tf
@@ -8,4 +8,22 @@ resource "yandex_compute_instance" "srv72-01" {
     cores  = 2
     memory = 2
   }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${var.centos-7-base}"
+      name        = "root-srv72-01"
+      type        = "network-nvme"
+      size        = "50"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.default.id}"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
---
+++ 07-terraform-02-syntax/src/terraform/variables.tf
@@ -0,0 +1,14 @@
+# ID облака YC
+variable "yandex_cloud_id" {
+  default = "b1gn303gl4s828djdqfi"
+}
+
+# Folder облака YC
+variable "yandex_folder_id" {
+  default = "b1gprhi83f991tp8b54f"
+}
+
+# ID образа созданого Packer
+variable "centos-7-base" {
+  default = "fd8hu92egb4b7or9b4gh"
+}
