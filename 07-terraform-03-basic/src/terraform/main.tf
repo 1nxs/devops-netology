d4320642d33335630fc085d32d3be7ba222307de
inxs <yakushin.pavel+netology@gmail.com>
2023-01-11 09:41:49 +0300

    update homework

---
+++ 07-terraform-03-basic/src/terraform/main.tf
@@ -0,0 +1,66 @@
+resource "yandex_compute_instance" "vm-count" {
+
+  name = "${terraform.workspace}-count-${count.index}"
+
+  resources {
+    cores  = local.vm_cores[terraform.workspace]
+    memory = local.vm_memory[terraform.workspace]
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${var.centos-7-base}"
+      name        = "root-srv73"
+      type        = "network-nvme"
+      size        = "20"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.default.id}"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  count = local.instance_count[terraform.workspace]
+}
+
+resource "yandex_compute_instance" "vm-4each" {
+
+  for_each = local.vm_foreach[terraform.workspace]
+  name = "${terraform.workspace}-foreach-${each.key}"
+
+  resources {
+    cores  = each.value.cores
+    memory = each.value.memory
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${var.centos-7-base}"
+      name        = "root-srv73"
+      type        = "network-nvme"
+      size        = "20"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.default.id}"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
--- 07-terraform-03-basic/src/terraform/variables.tf
+++ 07-terraform-03-basic/src/terraform/variables.tf
@@ -1,9 +1,35 @@
-# ID облака YC
 variable "yandex_cloud_id" {
   default = "b1gn303gl4s828djdqfi"
 }
 
-# Folder облака YC
 variable "yandex_folder_id" {
   default = "b1gprhi83f991tp8b54f"
+}
+
+variable "centos-7-base" {
+  default = "fd87hrmj6opa30n655a6"
+}
+
+locals {
+  instance_count = {
+    "prod"=2
+    "stage"=1
+  }
+  vm_cores = {
+    "prod"=2
+    "stage"=1
+  }
+  vm_memory = {
+    "prod"=2
+    "stage"=1
+  }
+  vm_foreach = {
+    prod = {
+      "3" = { cores = "2", memory = "2" },
+      "2" = { cores = "2", memory = "2" }
+    }
+	stage = {
+      "1" = { cores = "1", memory = "1" }
+    }
+  }
 }
