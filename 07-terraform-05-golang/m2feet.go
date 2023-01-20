c54830444e10ad7756b3cc3258b2ba5b622490ea
inxs <yakushin.pavel+netology@gmail.com>
2023-01-20 23:19:11 +0300

    update homework

--- 07-terraform-05-golang/m2feet.go
+++ 07-terraform-05-golang/m2feet.go
@@ -1,15 +1,14 @@
 package main
 
 import "fmt"
+// Задача 3_1
 
 //Пример из задания
 func task3_10() {
     fmt.Print("Enter a number: ")
     var input float64
     fmt.Scanf("%f", &input)
-
     output := input * 2
-
     fmt.Println(output)
 }
 
@@ -24,16 +23,16 @@ func task3_11() {
 	fmt.Printf("%v meters is: %4.4f feet.\n", input, output)
 }
 
+// Задача 3_2
 func task3_20() {
-// Помещаем строку из задачи 3_2
 	x := []int{48,96,86,68,57,82,63,70,37,34,83,27,19,97,9,17,}
 	min := x[0]
-	for y := range x {
+	for _,y := range x {
 		if y < min {
 			min = y
 		}
 	}
-	fmt.Println("Min integer is: \n",min)
+	fmt.Println("Min integer is:",min)
 }
 
 func main() {
